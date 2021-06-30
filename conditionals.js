fcc219d4580e74399f7277ff6083a8fa1fafbf94
bybang <bybang26@gmail.com>
2021-06-30 03:45:53 +0000

    Add more conditional examples

--- conditionals.js
+++ conditionals.js
@@ -37,3 +37,27 @@ if (temperature < -40 || temperature >40) {
 if (!raining) {
   console.log("Leave your umbrealla at home!");
 }
+
+const whichSchool = function (age) {
+  if (age >= 1 && age < 13) {
+    return "Elementary School";
+  }
+  else if (age >= 13 && age <= 18) {
+    return"Secondary School";
+  }
+  else if (age >= 19 && age <= 100) {
+    return "Lighthouse Labs";
+  }
+}
+console.log(whichSchool(8));
+console.log(whichSchool(14));
+console.log(whichSchool(35));
+
+var num = 99;
+
+while (num >= 1) {
+    if (num >= 95) {
+        console.log((num) + " bottles of juice on the wall! " + num + " bottles of juice! Take one down, pass it around... "+ (num-1) + " bottles of juice on the wall!");
+}
+num = num - 1
+}
